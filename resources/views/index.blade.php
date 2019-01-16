a27957031b5ea979a4c1b902c621bc95cb36142f
andrerobbe <mauritsrobbe@hotmail.com>
2019-01-16 03:54:45 +0100

    convert imgs

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,8 +6,9 @@
 <header class="overflow">
     <div class="slideshow">
         <div>
-            <img src="../img/header/party-dog.png" alt="A labrador dog that is in a party">
-            <img src="../img/header/dog-with-diving-glasses.png" alt="A dog that has swimming glasses equipped">
+            <img src="../img/header/party-dog.jpg" alt="A labrador dog that is in a party">
+            <img src="../img/header/dog-with-diving-glasses.jpg" alt="A dog that has swimming glasses equipped">
+            <img src="../img/header/dog-with-woman.jpg" alt="A dog that has swimming glasses equipped">
         </div>
     </div>
 </header>
