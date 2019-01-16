09abba174089980b09af2b438706b8413687eb39
andrerobbe <mauritsrobbe@hotmail.com>
2019-01-16 16:58:43 +0100

    added form to config/app.php

---
+++ resources/views/inc/slideshow.blade.php
@@ -0,0 +1,7 @@
+<div class="slideshow">
+	<div>
+		<img src="../img/header/party-dog.jpg" alt="A labrador dog that is in a party">
+		<img src="../img/header/dog-with-diving-glasses.jpg" alt="A dog that has swimming glasses equipped">
+		<img src="../img/header/dog-with-woman.jpg" alt="A dog that has swimming glasses equipped">
+    </div>
+</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="slideshow">
-	<div>
-		<img src="../img/header/party-dog.jpg" alt="A labrador dog that is in a party">
-		<img src="../img/header/dog-with-diving-glasses.jpg" alt="A dog that has swimming glasses equipped">
-		<img src="../img/header/dog-with-woman.jpg" alt="A dog that has swimming glasses equipped">
-    </div>
-</div>
 
-
+@include('inc.slideshow')
 <div class="wrapper">
-	
+	<div class="navigation">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam dicta cumque est quas reiciendis officiis iste consequatur sequi delectus autem asperiores veniam recusandae commodi, adipisci dolorum voluptas provident, rem quae!</p>
+		<ul>
+			<li class="sprite dog"><a href="/dogs">Dogs</a></li>
+			<li class="sprite dog"><a href="/cats">Cats</a></li>
+			<li class="sprite dog"><a href="/fish">Fish</a></li>
+			<li class="sprite dog"><a href="/birds">Birds</a></li>
+			<li class="sprite dog"><a href="/small-animals">Small Animals</a></li>
+			<li class=""><a href="/other">Other</a></li>
+		</ul>
+	</div>
+
+	<div class="hot-items">
+		<h1>Hot Items</h1>
+		<section>
+			<img src="" alt="">
+			<p>Cooling Mat</p>
+		</section>
+	</div>
+	<div class="subscribe">
+		<div class="bg-pattern">
+			<h2>Discover amazing Kowloon deals!</h2>
+			<p>only in our newsletter</p>			
+		</div>
+		<div>
+			<h3>Subscribe to our newsletter</h3>
+			<p>Lorem ipsum dolor sit amet</p>
+
+			{!! Form::open(['url' => '/submit', 'class' => 'form-controller']) !!}
+				{{Form::label('email', '')}}
+				{{Form::text('email', '', ['class' => '', 'placeholder' => "example@name.com", 'required', 'email'])}}
+				{{Form::submit('OK', ['class' => ''])}}
+			{!! Form::close() !!}
+
+		</div>
+	</div>
 </div>
 
 @endsection
