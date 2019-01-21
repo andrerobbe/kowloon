16098dabeeb4d8893a2d932380484a3e6bc2fdf8
andrerobbe <mauritsrobbe@hotmail.com>
2019-01-22 00:37:15 +0100

    fix homepage nav imgs

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,17 +10,17 @@
 	<div class="navigation">
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam dicta cumque est quas reiciendis officiis iste consequatur sequi delectus autem asperiores veniam recusandae commodi, adipisci dolorum voluptas provident, rem quae!</p>
 		<ul>
-			<li class="sprite dog"><a href="/dogs" class="align-center uppercase">Dogs</a></li>
+			<li class="sprite-big dog"><a href="/dogs" class="align-center uppercase">Dogs</a></li>
 			<li class="divider-vertical"></li>
-			<li class="sprite cat"><a href="/cats" class="align-center uppercase">Cats</a></li>
+			<li class="sprite-big cat"><a href="/cats" class="align-center uppercase">Cats</a></li>
 			<li class="divider-vertical"></li>
-			<li class="sprite fish"><a href="/fish" class="align-center uppercase">Fish</a></li>
+			<li class="sprite-big fish"><a href="/fish" class="align-center uppercase">Fish</a></li>
 			<li class="divider-vertical"></li>
-			<li class="sprite bird"><a href="/birds" class="align-center uppercase">Birds</a></li>
+			<li class="sprite-big bird"><a href="/birds" class="align-center uppercase">Birds</a></li>
 			<li class="divider-vertical"></li>
-			<li class="sprite hamster"><a href="/small-animals" class="align-center uppercase">Small Animals</a></li>
+			<li class="sprite-big hamster"><a href="/small-animals" class="align-center uppercase">Small Animals</a></li>
 			<li class="divider-vertical"></li>
-			<li class="sprite plus"><a href="/other" class="align-center uppercase">Other</a></li>
+			<li class="sprite-big plus"><a href="/other" class="align-center uppercase">Other</a></li>
 		</ul>
 	</div>
 
