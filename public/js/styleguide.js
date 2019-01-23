32024c7961f24152f5a648041d86a61bf196885d
andrerobbe <mauritsrobbe@hotmail.com>
2019-01-23 23:52:45 +0100

    styleguide buttons and input

--- public/js/styleguide.js
+++ public/js/styleguide.js
@@ -86,14 +86,171 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./resources/js/components/rangeSlider.js":
+/*!************************************************!*\
+  !*** ./resources/js/components/rangeSlider.js ***!
+  \************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+/*
+ *
+ * rangeSliders
+ *
+ */
+function getValues() {
+  // Get slider values
+  var parent = this.parentNode;
+  var sliders = parent.getElementsByTagName("input");
+  var slide1 = parseFloat(sliders[0].value);
+  var slide2 = parseFloat(sliders[1].value); // Neither slider will clip the other, so make sure we determine which is larger
+
+  if (slide1 > slide2) {
+    var tmp = slide2;
+    slide2 = slide1;
+    slide1 = tmp;
+  }
+
+  var displayValOne = parent.getElementsByClassName("rangeValue-1")[0];
+  var displayValTwo = parent.getElementsByClassName("rangeValue-2")[0];
+  displayValOne.innerHTML = slide1;
+  displayValTwo.innerHTML = slide2;
+} //
+
+
+window.onload = function () {
+  var sliderSections = document.getElementsByClassName("range-slider");
+
+  for (var i = 0; i < sliderSections.length; i++) {
+    var sliders = sliderSections[i].getElementsByTagName("input");
+
+    for (var y = 0; y < sliders.length; y++) {
+      if (sliders[y].type === "range") {
+        sliders[y].oninput = getValues; // Manually trigger event first time to display values
+
+        sliders[y].oninput();
+      }
+    }
+  }
+};
+
+/***/ }),
+
+/***/ "./resources/js/components/selectBox.js":
+/*!**********************************************!*\
+  !*** ./resources/js/components/selectBox.js ***!
+  \**********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+/*
+ *
+ * Select Box
+ *
+ */
+var x, i, j, selElmnt, a, b, c;
+/*look for any elements with the class "select-box":*/
+
+x = document.getElementsByClassName("select-box");
+
+for (i = 0; i < x.length; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  /*for each element, create a new DIV that will act as the selected item:*/
+
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  /*for each element, create a new DIV that will contain the option list:*/
+
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+
+  for (j = 1; j < selElmnt.length; j++) {
+    /*for each option in the original select element,
+    create a new DIV that will act as an option item:*/
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function (e) {
+      /*when an item is clicked, update the original select box,
+      and the selected item:*/
+      var y, i, k, s, h;
+      s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+      h = this.parentNode.previousSibling;
+
+      for (i = 0; i < s.length; i++) {
+        if (s.options[i].innerHTML == this.innerHTML) {
+          s.selectedIndex = i;
+          h.innerHTML = this.innerHTML;
+          y = this.parentNode.getElementsByClassName("same-as-selected");
+
+          for (k = 0; k < y.length; k++) {
+            y[k].removeAttribute("class");
+          }
+
+          this.setAttribute("class", "same-as-selected");
+          break;
+        }
+      }
+
+      h.click();
+    });
+    b.appendChild(c);
+  }
+
+  x[i].appendChild(b);
+  a.addEventListener("click", function (e) {
+    /*when the select box is clicked, close any other select boxes,
+    and open/close the current select box:*/
+    e.stopPropagation();
+    closeAllSelect(this);
+    this.nextSibling.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+  });
+}
+
+function closeAllSelect(elmnt) {
+  /*a function that will close all select boxes in the document,
+  except the current select box:*/
+  var x,
+      y,
+      i,
+      arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+
+  for (i = 0; i < y.length; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i);
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+
+  for (i = 0; i < x.length; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+/*if the user clicks anywhere outside the select box,
+then close all select boxes:*/
+
+
+document.addEventListener("click", closeAllSelect);
+
+/***/ }),
+
 /***/ "./resources/js/styleguide.js":
 /*!************************************!*\
   !*** ./resources/js/styleguide.js ***!
   \************************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
+__webpack_require__(/*! ./components/rangeSlider */ "./resources/js/components/rangeSlider.js");
 
+__webpack_require__(/*! ./components/selectBox */ "./resources/js/components/selectBox.js");
 
 /***/ }),
 
--- resources/views/layouts/styleguide.blade.php
+++ resources/views/layouts/styleguide.blade.php
@@ -41,24 +41,30 @@
 </head>
 <body>
     <div id="app">
+        <!-- Header -->
         <header>
             <h1 class="heading h-l">Styleguide</h1>
             <a href="/"><img src="/img/logo/kowloon-big.png" alt="kowloon logo"></a>
         </header>
-
+        
+        <!-- Navigation -->
         <ul class="nav">
             <li><a href="#colors">Colors</a></li>
             <li><a href="#logo">Logo</a></li>
             <li><a href="#icons">Icons</a></li>
             <li><a href="#typography">Typography</a></li>
             <li><a href="#buttons">Buttons</a></li>
+            <li><a href="#input">Input</a></li>
             <li><a href="/">Back to Kowloon</a></li>
         </ul>
 
         <main class="wrapper">
+
+
+
+            <!-- Colors -->
             <div id="colors">
                 <h2 class="heading h-l">Colors</h2>
-
                 <div>
                     <div class="colors accent">
                         <h3>Accent Colors</h3>
@@ -95,9 +101,14 @@
                     </div>
                 </div>
             </div>
+
+
+
+            <!-- Logo -->
             <div id="logo">
                 <h2 class="heading h-l">Logo</h2>
-                <div class="logos">
+                <p>You can use the following logos. Note that the 'K' logo color depends on the animal category and can only be used within the right category. For example the purple K can only be used on pages about dogs.</p>
+                <div class="logos margin-top">
                     <div class="flex">
                         <img src="/img/logo/kowloon-big.png" alt="kowloon logo">
                         <img src="/img/logo/kowloon.png" alt="kowloon logo" class="kowloon">
@@ -113,8 +124,13 @@
                 </div>
                 
             </div>
+
+
+
+            <!-- Icons -->
             <div id="icons">
                 <h2 class="heading h-l">Icons</h2>
+                <p>We use these icons to display the animal categories.</p>
                 <div class="icons flex">
                     <div class="sprite-big dog"></div>
                     <div class="sprite-big cat"></div>
@@ -125,17 +141,18 @@
                 </div>
                 <div class="code">
 <xmp>
-<div class="sprite-big dog"></div>
-<div class="sprite-big cat"></div>
-<div class="sprite-big fish"></div>
-<div class="sprite-big bird"></div>
+<div class="sprite-big dog">    </div>
+<div class="sprite-big cat">    </div>
+<div class="sprite-big fish">   </div>
+<div class="sprite-big bird">   </div>
 <div class="sprite-big hamster"></div>
-<div class="sprite-big plus"></div>
+<div class="sprite-big plus">   </div>
 
-To make the icon white add a filter:
+To make the icon white, add a filter
 <div class="sprite-big dog" style="filter: brightness(10) saturate(0);"></div>
 </xmp>
                 </div>
+                <p class="margin-top">These icons are used for navigation purposes.</p>
                 <div class="icons flex small">
                     <div class="sprite hamburger"></div>
                     <div class="sprite search"></div>
@@ -147,27 +164,31 @@ To make the icon white add a filter:
                 </div>                
                 <div class="code">
 <xmp>
-<div class="sprite hamburger"></div>
-<div class="sprite search"></div>
-<div class="sprite faq"></div>
-<div class="sprite break"></div>
-<div class="sprite clear"></div>
-<div class="sprite esc"></div>
-<div class="sprite mail"></div>
+<div class="sprite hamburger">  </div>
+<div class="sprite search">     </div>
+<div class="sprite faq">        </div>
+<div class="sprite break">      </div>
+<div class="sprite clear">      </div>
+<div class="sprite esc">        </div>
+<div class="sprite mail">       </div>
 </xmp>
                 </div>
             </div>
+
+
+
+            <!-- Typography -->
             <div id="typography">
                 <h2 class="heading h-l">Typography</h2>
                 <p class="font-title">The font used for headings is Choplin Demo</p>
                 <p class="font-txt">The font used for regular text is Arial</p>
-                <div>
+                <div class="margin-top">
                     <h3 class="heading h-xl uppercase">H1 44 pixels</h1>
                     <h3 class="heading h-xl">H1 44 pixels</h1>
                     <h3 class="heading h-xl font-txt">Arial 44px</h1>
                     <h3 class="heading h-l">H2 40px</h1>
                     <h3 class="heading h-ml">H3 26px</h1>
-                    <h3 class="heading h-m">H1 20px</h1>
+                    <h3 class="heading h-m">H4 20px</h1>
                     <p class="nav-m align-center">Nav 22px</p>
                     <p class="btn-m align-center">Button 18.97px</p>
                     <p class="txt-xxl align-right">Txt 20px</p>
@@ -182,31 +203,253 @@ To make the icon white add a filter:
 <p class="font-txt">The font used for regular text is Arial</p>
 
 
-<h3 class="heading h-xl uppercase">H1 44 pixels</h1>
-<h3 class="heading h-xl">H1 44 pixels</h1>
-<h3 class="heading h-xl font-txt">Arial 44px</h1>
-<h3 class="heading h-l">H2 40px</h1>
-<h3 class="heading h-ml">H3 26px</h1>
-<h3 class="heading h-m">H1 20px</h1>
+<h3 class="heading h-xl uppercase">         H1 44 pixels    </h1>
+<h3 class="heading h-xl">                   H1 44 pixels    </h1>
+<h3 class="heading h-xl font-txt">          Arial 44px      </h1>
+<h3 class="heading h-l">                    H2 40px         </h1>
+<h3 class="heading h-ml">                   H3 26px         </h1>
+<h3 class="heading h-m">                    H4 20px         </h1>
 
-<p class="nav-m align-center">Nav 22px</p>
-<p class="btn-m align-center">Button 18.97px</p>
+<p class="nav-m align-center">              Nav 22px        </p>
+<p class="btn-m align-center">              Button 18.97px  </p>
 
-<p class="txt-xxl align-right">Txt 20px</p>
-<p class="txt-xl align-right">Txt 16px</p>
-<p class="txt-l align-right">Txt 15px</p>
-<p class="txt-m align-right">Txt 14px</p>   
-<p class="txt-m font-title align-right">Choplin 14px</p> 
+<p class="txt-xxl align-right">             Txt 20px        </p>
+<p class="txt-xl align-right">              Txt 16px        </p>
+<p class="txt-l align-right">               Txt 15px        </p>
+<p class="txt-m align-right">               Txt 14px        </p>   
+<p class="txt-m font-title align-right">    Choplin 14px    </p> 
 </xmp>
                 </div>
                 
             </div>
+
+
+
+            <!-- Buttons -->
             <div id="buttons">
                 <h2 class="heading h-l">Buttons</h2>
-                
+                <p>The color of the button depends on which page you're on.</p>
+                <div class="flex">
+                    <button class="btn btn-primary">Regular Button</button>
+                    <button class="btn btn-primary btn-cookie">Cookie Button</button>
+                </div>
+                <div class="flex margin-top">
+                    <button class="btn btn-primary dog">Dog Button</button>
+                    <button class="btn btn-primary cat">Cat Button</button>
+                    <button class="btn btn-primary fish">Fish Button</button>
+                    <button class="btn btn-primary bird">Bird Button</button>
+                    <button class="btn btn-primary hamster">Hamster Button</button>
+                    <button class="btn btn-primary other">Other Button</button>                    
+                </div>
+                <div class="code">
+<xmp>
+<button class="btn btn-primary">            Regular Button  </button>
+<button class="btn btn-primary btn-cookie"> Cookie Button   </button> //uses transparent BG on hover
+<button class="btn btn-primary dog">        Dog Button      </button>
+<button class="btn btn-primary cat">        Cat Button      </button>
+<button class="btn btn-primary fish">       Fish Button     </button>
+<button class="btn btn-primary bird">       Bird Button     </button>
+<button class="btn btn-primary hamster">    Hamster Button  </button>
+<button class="btn btn-primary other">      Other Button    </button>
+
+
+CSS used on buttons above:
+
+button {
+    display: block;
+    margin: 10px 0;
+    width: 250px;
+    height: 50px;
+}
+
+</xmp>
+                </div>
             </div>
-        </main>        
+
+
+
+            <!-- Input -->
+            <div id="input">
+                <h2 class="heading h-l">Input</h2>
+
+                <!-- Checkboxes -->
+                <div class="checkboxes">
+                    <h3>Checkboxes</h3>
+                    <div class="article-filters">
+                        <h4>Checkboxes Regular</h4>
+                        <section class="categories">
+                            <label class="checkbox">Checked
+                                <input type="checkbox" checked="">
+                                <span class="checkmark"></span>
+                            </label>
+                            <label class="checkbox">Not Checked
+                                <input type="checkbox">
+                                <span class="checkmark"></span>
+                            </label>
+                        </section>
+                    </div>
+                    <div class="checkbox-grid">
+                        <div class="article-filters dog">
+                            <h4>Checkboxes Dog Pages</h4>
+                            <section class="categories">
+                                <label class="checkbox">Checked
+                                    <input type="checkbox" checked="">
+                                    <span class="checkmark"></span>
+                                </label>
+                                <label class="checkbox">Try Hovering
+                                    <input type="checkbox">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </section>
+                        </div>
+                        <div class="article-filters cat">
+                            <h4>Checkboxes Cat Pages</h4>
+                            <section class="categories">
+                                <label class="checkbox">Checked
+                                    <input type="checkbox" checked="">
+                                    <span class="checkmark"></span>
+                                </label>
+                                <label class="checkbox">Try Hovering
+                                    <input type="checkbox">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </section>
+                        </div>
+                        <div class="article-filters fish">
+                            <h4>Checkboxes Fish Pages</h4>
+                            <section class="categories">
+                                <label class="checkbox">Checked
+                                    <input type="checkbox" checked="">
+                                    <span class="checkmark"></span>
+                                </label>
+                                <label class="checkbox">Try Hovering
+                                    <input type="checkbox">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </section>
+                        </div>
+                        <div class="article-filters bird">
+                            <h4>Checkboxes Bird Pages</h4>
+                            <section class="categories">
+                                <label class="checkbox">Checked
+                                    <input type="checkbox" checked="">
+                                    <span class="checkmark"></span>
+                                </label>
+                                <label class="checkbox">Try Hovering
+                                    <input type="checkbox">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </section>
+                        </div>
+                        <div class="article-filters hamster">
+                            <h4>Checkboxes Hamster Pages</h4>
+                            <section class="categories">
+                                <label class="checkbox">Checked
+                                    <input type="checkbox" checked="">
+                                    <span class="checkmark"></span>
+                                </label>
+                                <label class="checkbox">Try Hovering
+                                    <input type="checkbox">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </section>
+                        </div>
+                        <div class="article-filters other">
+                            <h4>Checkboxes Other Pages</h4>
+                            <section class="categories">
+                                <label class="checkbox">Checked
+                                    <input type="checkbox" checked="">
+                                    <span class="checkmark"></span>
+                                </label>
+                                <label class="checkbox">Try Hovering
+                                    <input type="checkbox">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+                <div class="code">
+<xmp>
+Change the class 'dog' in the div accordingly to: cat, fish, bird, hamster, .other
+
+<div class="article-filters dog">
+    <section class="categories">
+        <label class="checkbox">Checked
+            <input type="checkbox" checked="">
+            <span class="checkmark"></span>
+        </label>
+        <label class="checkbox">Not Checked
+            <input type="checkbox">
+            <span class="checkmark"></span>
+        </label>
+    </section>
+</div>
+</xmp>
+                </div>
+
+
+                <!-- Priceslider -->
+                <h3 class="margin-top">Price slider</h3>
+                <div class="price-slider">
+                    <section class="range-slider">
+                        <input value="8" min="8" max="499" step="1" type="range">
+                        <input value="499" min="8" max="499" step="1" type="range">
+                        <div class="values">
+                            <div class="val-1"><span class="euro-sign">&euro;</span><span class="rangeValue-1"></span></div> - <div class="val-2"><span class="euro-sign">&euro;</span><span class="rangeValue-2"></span></div>
+                        </div>
+                    </section>
+                </div>
+                <div class="code">
+<xmp>
+<div class="price-slider">
+    <section class="range-slider">
+        <input value="8" min="8" max="499" step="1" type="range">
+        <input value="499" min="8" max="499" step="1" type="range">
+        <div class="values">
+            <div class="val-1">
+                <span class="euro-sign">&euro;</span><span class="rangeValue-1"></span>
+            </div>
+             - 
+             <div class="val-2">
+                <span class="euro-sign">&euro;</span><span class="rangeValue-2"></span>
+            </div>
+        </div>
+    </section>
+</div>
+</xmp>
+                </div>
+                
+                <!-- Select Box -->
+                <h3 class="margin-top">Select Options</h3>
+                <div class="code">
+<xmp>
+<div class="select-box">
+    <select>
+        <option>Sort by relevance</option>
+        <option>Price: low to high</option>
+        <option>Price: high to low</option>
+        <option>Latest</option>
+        <option>Oldest</option>
+    </select>
+</div>
+</xmp>
+                </div>
+                <div class="select-box">
+                    <select>
+                        <option>Sort by relevance</option>
+                        <option>Price: low to high</option>
+                        <option>Price: high to low</option>
+                        <option>Latest</option>
+                        <option>Oldest</option>
+                    </select>
+                </div>
+            </div>
+            <!-- End of Input -->
+        </main>
     </div>
+
+
     <!-- Scripts -->
     <script src="{{ asset('js/styleguide.js') }}" defer></script>
 </body>
